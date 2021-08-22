2d071c77b2a6f9e2334c6e6966a9d4f61101602d
ShahriarAsif09 <shariarasif2002@gmail.com>
2021-08-22 10:17:43 -0700

    my first c programming

---
+++ cm.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int roll, inch;
+float cm;
+
+printf("Integer value for inches;");
+scanf("%d",&inch);
+cm=inch*2.54;
+printf("/n centimeter=%f",cm);
+getch();
+}
